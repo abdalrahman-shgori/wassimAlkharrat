8acad337a548c2561014488556ff0190cd1d1e22
Abdalrahmansh9 <abdalrahman.job9@gmail.com>
2025-12-07 13:17:49 +0300

    update

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -4,6 +4,9 @@ import LogoutButton from "./LogoutButton";
 import styles from "./layout.module.scss";
 import "../../globals.css";
 
+// Force dynamic rendering since we use cookies for authentication
+export const dynamic = 'force-dynamic';
+
 export default async function AdminLayout({
   children,
 }: {
